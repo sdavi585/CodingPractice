16bee49e5b87a2b653946592c38c2abb717bded2
Stella David <sdavi585@mtroyal.ca>
2023-09-29 23:16:16 -0600

    Added Ch2's practice set (variable types)

---
+++ CHW_Ch2_PracticeSet.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class CHW_Ch2_PracticeSet {
+    public static void main(String[] args) {
+//--------------QUESTION 1-----------------------------------------------//
+//        float a = 7/4;
+//        System.out.println(a);
+//
+//--------------QUESTION 2-----------------------------------------------//
+//        char grade = 'B';
+//        grade = (char) (grade + 8);
+//        System.out.println(grade);
+//
+//        char decrypt = (char) (grade - 8);
+//        System.out.println(decrypt);
+//
+// --------------QUESTION 3-----------------------------------------------//
+//        Scanner sc = new Scanner(System.in);
+//        int guessNumber = 7; we dont need this line! Simply put the
+//        number like in line 25.
+//
+//        System.out.println("Guess a number: ");
+//        int userGuess = sc.nextInt();
+//
+//        System.out.println(userGuess > 7);
+//
+// --------------QUESTION 4-----------------------------------------------//
+//        float v, u, a, s;
+//        v = 6;
+//        u = 4;
+//        a = 3;
+//        s = 8;
+//        float result = (v*2 - u*2)/ 2 * a * s;
+//
+// --------------QUESTION 5-----------------------------------------------//
+//        int a = (7 * 49/7) + (35/7);
+//        System.out.println(a);
+//
+    }
+}
