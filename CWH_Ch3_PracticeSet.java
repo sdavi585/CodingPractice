ab693ab47a43bb905ea12bbd1edb361233217c94
Stella David <sdavi585@mtroyal.ca>
2023-09-29 23:30:22 -0600

    Ch3 practice set (Java string methods)

---
+++ CWH_Ch3_PracticeSet.java
@@ -0,0 +1,31 @@
+public class CWH_Ch3_PracticeSet {
+    public static void main(String[] args) {
+//--------------QUESTION 1-----------------------------------------------//
+//        String name = "Stella";
+//        name = name.toLowerCase();
+//        System.out.println(name);
+
+// --------------QUESTION 2-----------------------------------------------//
+//         String name = "Stella Rebecca David";
+//         name = name.replace(" ", "_");
+//        System.out.println(name);
+
+// --------------QUESTION 3-----------------------------------------------//
+//        String letter = "Dear Stella, Thanks a lot.";
+//        System.out.println(letter);
+//
+//        letter = letter.replace("Stella", "David");
+//        System.out.println(letter);
+
+// --------------QUESTION 4-----------------------------------------------//
+//         String name = "Stella  Rebecca   David";
+//
+//        System.out.println(name.indexOf("  "));
+//        System.out.println(name.indexOf("   "));
+//
+
+// --------------QUESTION 5-----------------------------------------------//
+//        String letter = "Dear Harry, This Java Course is nice. Thanks.";
+
+    }
+}
