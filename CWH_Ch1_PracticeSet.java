0adf6cae369973eaecea785faf91994890cc1128
Stella David <sdavi585@mtroyal.ca>
2023-09-29 23:34:11 -0600

    Ch1 practice set (Java variables, data types & Usr input)

---
+++ CWH_Ch1_PracticeSet.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class CWH_Ch1_PracticeSet {
+    public static void main(String[] args) {
+
+//--------------QUESTION 1-----------------------------------------------//
+//        float num1 = 3;
+//        float num2 = 3;
+//        float num3 = 3;
+//
+//        float sum = num1 + num2 + num3;
+//        System.out.println("The sum of 3 numbers is " + sum);
+//
+//
+// --------------QUESTION 2-----------------------------------------------////
+//        float sub1 = 78;
+//        float sub2 = 89;
+//        float sub3 = 88;
+//
+//        float finalMarks = (sub1 + sub2 + sub3) / 3;
+//        System.out.println("Your final marks are " + finalMarks + " percent.");
+//
+// --------------QUESTION 3-----------------------------------------------//
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name + ", have a good day.");
+
+// --------------QUESTION 4-----------------------------------------------//
+//        double kilometer = 134;
+//        double mile = kilometer/1.609;
+//
+//        System.out.println(kilometer + " kilometers in miles is " + mile);
+//
+// --------------QUESTION 5-----------------------------------------------//
+//        boolean aNumber;
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter a number");
+//        System.out.println(sc.hasNextInt());
+    }
+}
