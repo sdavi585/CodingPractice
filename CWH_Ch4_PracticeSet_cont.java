0ed47503b49bff152caa08641eebcc6ef82fbce2
Stella David <sdavi585@mtroyal.ca>
2023-12-19 13:08:41 -0700

    Question 4 & 5 answers for if else statements

---
+++ CWH_Ch4_PracticeSet_cont.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CWH_Ch4_PracticeSet_cont {
+    public static void main(String[] args) {
+
+//--------------QUESTION 4-----------------------------------------------//
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter year: ");
+//        int year = scn.nextInt();
+//
+//        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+//            System.out.println(year + " is a leap year.");
+//        } else {
+//            System.out.println(year + " is not a leap year.");
+//        }
+
+//--------------QUESTION 5-----------------------------------------------//
+//        Scanner scn = new Scanner(System.in);
+//
+//        System.out.println("Enter link: ");
+//        String link = scn.nextLine();
+//
+//        if (link.endsWith(".org")) {
+//            System.out.println(link + " is an organizational website.");
+//
+//        } else if (link.endsWith(".com")) {
+//            System.out.println(link + " is a commercial website.");
+//
+//        } else if (link.endsWith(".ca")) {
+//            System.out.println(link + " is a canadian website.");
+//
+//        } else {
+//            System.out.println("Unknown website entered.");
+//        }
+    }
+}
